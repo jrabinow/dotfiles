e60121a2fa93417c23da1778782820bbb0835e34
Julien Rabinow <jnr305@nyu.edu>
2017-10-28 01:58:45 -0700

    Post-facebook commit

---
+++ .bashrc
@@ -0,0 +1,94 @@
+umask 022
+
+# Unlike earlier versions, Bash4 sources your bashrc on non-interactive shells.
+# The line below prevents anything in this file from creating output that will
+# break utilities that use ssh as a pipe, including git and mercurial.
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# Source global definitions as early as possible so we can overwrite everything
+# if need be, instead of the opposite
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
+HISTFILE=$HOME/.bash_history
+
+# keep the facebook module first: it sources global config stuff we wanna
+# overwrite later
+MODULE_NAMES=(
+    # facebook  
+    aliases
+    bookmarks
+    envvars
+    functions_generic
+    pathmods
+    ps1
+    scm_prompt_setup
+    shellconfig
+)
+
+case $(uname) in
+    Linux)
+        MODULE_NAMES=(${MODULE_NAMES[@]} linux)
+        ;;
+    Darwin)
+        MODULE_NAMES=(${MODULE_NAMES[@]} mac_os)
+        ;;
+esac
+
+MODULE_NAMES=(${MODULE_NAMES[@]} fortune) # load this guy last
+
+BASH_MOD_DIR="${HOME}/.config/bash"
+
+function missing_module ()
+{
+    local module="$1"; shift
+    if [ -z "$(grep -E "^\s*$module  # __IGNORE_INTERNAL__" \
+            $HOME/.bashrc)" ]; then
+        while [[ ! "$REPLY" =~ d|D|i|I|r|R ]]; do
+            read -n1 -p '[D]isable? [R]emove? [I]gnore? '
+            echo
+        done
+        case "${REPLY}" in
+            d|D)  # disable by commenting out module
+                sed -i -e "s/^\(\s*\)$module$/\1# $file/" $HOME/.bashrc
+                ;;
+            i|I)  # show warning but disable prompt
+                sed -i -e \
+                    "s/^\(\s*\)$module$/\1$module  # __IGNORE_INTERNAL__/" \
+                    $HOME/.bashrc
+                ;;
+            r|R) # remove module from module list
+                sed -i -e "/^\s*$module$/d" $HOME/.bashrc
+                ;;
+        esac
+    fi
+}
+
+function load_modules ()
+{
+    for module in "${MODULE_NAMES[@]}"; do
+        local file_path="${BASH_MOD_DIR}/${module}"
+        if [ -f "${file_path}" ] || [ -L "${file_path}" ]; then
+            source "${file_path}"
+        else
+            echo "Missing module '${module}'" >&2
+            #missing_module "${module}"
+        fi
+    done
+    unset module
+}
+
+load_modules
+unset MODULE_NAMES
+unset missing_module
+unset load_modules
+
+# this goes here because putting it in another file means bash ignores it for
+# some reason :-(
+HISTSIZE=30000
+HISTFILESIZE=30000
+# don't put duplicate lines in the history
+HISTCONTROL=ignoreboth
--- setup.sh
+++ setup.sh
@@ -2,98 +2,60 @@
 
 function main ()
 {
-	readonly LOGFILE=$(mktemp --tmpdir $(basename $0)-log.XXXXXX)
-	CLEAN=false
-	MOVE=false
-	mvcmd="ln -s"
 	readonly files=(
-		bashrc
-		gdb-dashboard/.gdbinit
-		gitconfig
-		muttrc
-		profile
-		tmux.conf
-		vimrc
-		vim
+		.bashrc
+		.config/bash
+		.ctags
+		.gdbinit
+		.gitconfig
+		.hgrc
+		.inputrc
+		.profile
+		.root-env
+		.tmux.conf
+		.tmuxinator
+		.vim
+		.vimrc
 	)
-	readonly config_files=(
-		git-prompt.sh
-	)
+	PREPARE_COMMIT=false
 
-#	GROUP=
-#	MODE=644
-#	VERBOSE=false
-#	while getopts "cghmv" opt; do
-
-	while getopts "mch" opt; do
-		case ${opt} in
-			c)
-				CLEAN=true
+	while getopts "hp" opt; do
+		case "${opt}" in
+			p)
+				PREPARE_COMMIT=true
 				;;
 			h)
 				cat << EOF
 Usage: $(basename $0) [OPTION]...
-Options: -c: cleanup everything from git repo that isn't a dotfile
-	 -h: show this help dialog
-	 -m: move dotfiles instead of symlinking them in home directory
+Options: -h: display this help message
+	 -p: prepare new commit by copying all files to repo dir
 EOF
 				exit 0
 				;;
-			m)
-				MOVE=true
-				;;
 			?)
-				echo "Unknown option, exiting now" | tee -a "${LOGFILE}" >&2
+				echo "Unknown option, exiting now"
 				exit 1
 				;;
-#			g)
-#				GROUP="${OPTARG}"
-#				;;
-#Usage: $(basename $0) [OPTION]...
-#Options: -c: cleanup everything from git repo that isn't a dotfile
-#	 -g ARG: set group to ARG
-#	 -h: show this help dialog
-#	 -m ARG: set chown bits mode to ARG
-#	 -v: verbose mode
-#				MODE="${OPTARG}"
-#				;;
-#			v)
-#				VERBOSE=true
-#				;;
 		esac
 	done
-	shift $((OPTIND-1))
-	if "${CLEAN}"; then
-		MOVE=true
-	fi
-	if "${MOVE}"; then
-		mvcmd="mv"
-	fi
 
-	for f in "${files[@]}"; do
-		filename="$(basename "${f}" | sed -e 's/^[^[:punct:]]/.&/')"
-		${mvcmd} -v "$(pwd)/${f}" "${HOME}/${filename}" 2>&1 | tee -a "${LOGFILE}" >&2
-	done
-	mkdir -vp "$HOME/.config" 2>&1 | tee -a "${LOGFILE}" >&2
-	for f in "${config_files[@]}"; do
-		filename="$(basename "${f}")" #| sed -e 's/^[^[:punct:]]/.&/')"
-		${mvcmd} -v "$(pwd)/${f}" "${HOME}/.config/${filename}" 2>&1 | tee -a "${LOGFILE}" >&2
-	done
-
-	if "${CLEAN}"; then
-		dir=$(basename $(pwd))
-		cd ..
-		rm -rf "${dir}"
+	if ${PREPARE_COMMIT}; then
+		for f in "${files[@]}"; do
+			dir="$(dirname "${f}")"
+			file="$(basename "${f}")"
+			mkdir -p "./${dir}"
+			rm -r "./${dir}/${file}"
+			cp -rv "$HOME/$f" "./${dir}/"
+		done
+	else
+		for f in "${files[@]}"; do
+			dir="$(dirname "${f}")"
+			file="$(basename "${f}")"
+			mkdir -p "${HOME}/${dir}"
+			rm -r "${HOME}/${dir}/${file}"
+			cp -rv "$f" "${HOME}/${dir}/"
+		done
 	fi
-#	install_args="-d --mode=${MODE}"
-#	if [ ! -z "${GROUP}" ]; then
-#		install_args+=" --group=${GROUP}"
-#	fi
-#	if "${VERBOSE}"; then
-#		install_args+=" -v"
-#	fi
-#		install ${install_args} "${f}" "${HOME}/.config/${filename}" | tee -a "${LOGFILE}" >&2
-#		install ${install_args} "${f}" "${HOME}/${filename}" | tee -a "${LOGFILE}" >&2
 }
 
 if [ "${BASH_SOURCE[0]}" == "$0" ]; then
