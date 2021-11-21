8d492987b7ba0ed8394e3c3441772886faa193cd
Julien Rabinow <randprefix.github@fastmail.com>
2021-11-20 20:35:53 -0800

    gdb ubuntu fix

--- setup.sh
+++ setup.sh
@@ -120,7 +120,7 @@ function setup_homedir()
     fi
     if command -v gdb > /dev/null; then
         # if gdb version less than 11.0
-        gdb_version="$(gdb --version|head -1|sed -En 's/^GNU gdb (\(GDB\) )?([0-9]+)\.([0-9]+)?/\2\3/p')"
+        gdb_version="$(gdb --version|head -1|sed -En 's/^GNU gdb (.* )?([0-9]+)\.([0-9]+)?(\.[0-9]+)*$/\2\3/p')"
         if ((${gdb_version} < 110 )) && [ ! -L ~/.gdbinit ]; then
             ln -s ~/.config/gdb/gdbinit ~/.gdbinit
         fi
